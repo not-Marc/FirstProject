36e32ae02d3a844f187f95eae2285d0cda4318a4
not-Marc <43054897+not-Marc@users.noreply.github.com>
2018-09-24 20:30:39 -0400

    Add files via upload

---
+++ FavoriteSubjects.java
@@ -0,0 +1,11 @@
+
+public class FavoriteSubjects 
+{
+	public static void main(String[] args)
+	{
+		System.out.print("My favorite subjects are:\n\n");
+		System.out.print("\tComputer Science\n\n");
+		System.out.print("\tMath\n\n");
+		System.out.print("\tHistory");
+	}
+}
